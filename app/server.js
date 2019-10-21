f16207c886224eb10025a437418130dfd919ca33
Merav <merav.mi@gmail.com>
2019-10-21 19:13:04 +0300

    receiving data from mongodb

--- app/server.js
+++ app/server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongojs = require('mongojs');
-const db = mongojs('doglist', ['dogs']);
-const mongodb = require('mongodb');
-const fs = require('fs').promises;
-const fetch = require('node-fetch');
+const db = mongojs('doglist', ['dogs']); //doglist = db, dogs = collection
+// const mongodb = require('mongodb');
+// const fs = require('fs').promises;
+// const fetch = require('node-fetch');
 const app = express();
 const port = 5555;
 
@@ -15,26 +15,11 @@ app.listen(port, () => {
     console.log(`Dog Medical Treatment App is running on port: ${port}`);
 })
 
+app.get('/doglist', function(req, res) { // doglist = db
+    console.log("GET request received");
 
-const initialDogList = [{
-        name: "סנופי",
-        age: 4,
-        num: 2562,
-        gender: "זכר",
-        breed: "מעורב",
-        loc: "כלבייה",
-        status: "טופל",
-        descrip: "נמצא נטוש בירכא"
-    },
-    {
-        name: "לאקי",
-        age: 1,
-        num: 2575,
-        gender: "נקבה",
-        breed: "קוקר ספנייל",
-        loc: "אומנה",
-        status: "פתוח",
-        descrip: "אושפזה עקב קדחת קרציות"
-    }
-];
-
+    db.dogs.find(function(err, docs) { // dogs = collection
+        console.log(docs);
+        res.json(docs);        
+    });
+});
