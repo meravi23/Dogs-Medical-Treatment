9c4d3864949ee03a8e57e9d9581fd6a0f7258498
Merav <merav.mi@gmail.com>
2019-10-21 18:43:56 +0300

    loaded mongo.js

--- app/server.js
+++ app/server.js
@@ -1,34 +1,40 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongojs = require('mongojs');
+const db = mongojs('doglist', ['dogs']);
+const mongodb = require('mongodb');
+const fs = require('fs').promises;
+const fetch = require('node-fetch');
 const app = express();
 const port = 5555;
 
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
 
 app.listen(port, () => {
     console.log(`Dog Medical Treatment App is running on port: ${port}`);
 })
 
-app.get('/dog-list', function (req, res) {
-    console.log("receiving get request");
-    dog1 = {
+
+const initialDogList = [{
         name: "סנופי",
         age: 4,
         num: 2562,
         gender: "זכר",
+        breed: "מעורב",
         loc: "כלבייה",
         status: "טופל",
         descrip: "נמצא נטוש בירכא"
-    };
-    dog2 = {
+    },
+    {
         name: "לאקי",
         age: 1,
         num: 2575,
         gender: "נקבה",
+        breed: "קוקר ספנייל",
         loc: "אומנה",
         status: "פתוח",
         descrip: "אושפזה עקב קדחת קרציות"
     }
+];
 
-    let dogList = [dog1, dog2];
-    res.json(dogList);
-});
