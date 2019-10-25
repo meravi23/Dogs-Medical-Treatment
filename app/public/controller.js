b661159eebcdcbcca1f48bd4a67b08c612c672fa
Merav <merav.mi@gmail.com>
2019-10-25 10:06:51 +0300

    various changes

--- app/public/controller.js
+++ app/public/controller.js
@@ -2,38 +2,61 @@ var app = angular.module("dogsMed", ['ui.bootstrap']).controller('AppCtrl', func
     $scope.name = "";
     $scope.num = null;
     $scope.status = "";
+    $scope.gender = "";
     $scope.dogs = [];
 
 
     let refresh = function () {
-        $http.get('/doglist').then(function (res) {
+        $http.get('/dogs').then(function (res) {
             console.log(res.data);
             $scope.dogs = res.data;
-            $scope.dog = null;
         });
     };
     refresh();
 
-
-
     $scope.addDog = function () {
         $scope.dog._id = null; // this way whenever a new contact is added it is always assigned a new id by the database
         console.log($scope.dog);
-        $http.post('/doglist', $scope.dog).then(function (res) {
+        $http.post('/dogs', $scope.dog).then(function (res) {
             console.log(res);
             refresh();
         });
     };
 
+    $scope.edit = function (id) {
+        console.log(id);
+        $http.get('/dogs/' + id).then(function(res) {
+          $scope.dog = res.data;
+        });
+    } 
+    
+    $scope.update = function () {
+        console.log($scope.dog._id);
+        $http.put('/dogs/' + $scope.dog._id, $scope.dog)
+            .then(function (res) {
+                refresh();
+            });
+    };
 
-    $scope.filterByName = function (dog) {
-        if ($scope.name === "") {
-            return true;
-        } else if (dog.name.startsWith($scope.name)) {
-            return true;
-        } else {
-            return false;
-        }
+    $scope.remove = function (id) {
+        console.log(id);
+        $http.delete('/dogs/' + id).then(function (res) {
+            refresh();
+        });
     }
+
+    $scope.clear = function () {
+        $scope.dog = null;
+    };
+
+    // $scope.filterByName = function (dog) {
+    //     if ($scope.name === "") {
+    //         return true;
+    //     } else if (dog.name.startsWith($scope.name)) {
+    //         return true;
+    //     } else {
+    //         return false;
+    //     }
+    // }
 
 });
--- app/server.js
+++ app/server.js
@@ -17,18 +17,72 @@ app.listen(port, () => {
     console.log(`Dog Medical Treatment App is running on port: ${port}`);
 })
 
-app.get('/doglist', function(req, res) { // doglist = db
+app.get('/dogs', function(req, res) {
     console.log("GET request received");
 
+    // dog1 = {
+    //     name: "סנופי",
+    //     age: 4,
+    //     num: 2562,
+    //     gender: "זכר",
+    //     loc: "כלבייה",
+    //     status: "טופל",
+    //     descrip: "נמצא נטוש בירכא"
+    // };
+    // dog2 = {
+    //     name: "לאקי",
+    //     age: 1,
+    //     num: 2575,
+    //     gender: "נקבה",
+    //     loc: "אומנה",
+    //     status: "פתוח",
+    //     descrip: "אושפזה עקב קדחת קרציות"
+    // }
+
+    // let dogList = [dog1, dog2];
+    // res.json(dogList);
+
     db.dogs.find(function(err, docs) { // dogs = collection
-        // console.log(docs);
+        console.log(docs);
         res.json(docs);        
     });
 });
 
-app.post('/doglist', function(req, res) {
+
+app.post('/dogs', function(req, res) {
     console.log(req.body);
     db.dogs.insert(req.body, function(err, doc) {
         res.json(doc);
     });
 });
+
+app.get('/dogs/:id', function(req, res) {
+    let id = req.params.id;
+    console.log(id);
+    db.dogs.findOne({_id: mongojs.ObjectId(id)}, function(err, doc) {
+        res.json(doc);
+    });
+});
+
+app.put('/dogs/:id', function(req, res) {
+    let id = req.params.id;
+    console.log(req.body.name);
+    db.dogs.findAndModify({query: {_id: mongojs.ObjectId(id)},
+        update: {$set: {name: req.body.name, age: req.body.age,
+             num: req.body.num, gender: req.body.gender, loc: req.body.loc,
+            status: req.body.status, descrip: req.body.descrip, rabbies: req.body.rabbies,
+            distemper1: req.body.distemper1, distemper2: req.body.distemper2, distemper3: req.body.distemper3,
+            neuter: req.body.neuter}},
+        new: true}, function(err, doc) {
+            res.json(doc);
+    });
+});
+
+app.delete('/dogs/:id', function(req, res) {
+    let id = req.params.id;
+    console.log(id);
+    db.dogs.remove({_id: mongojs.ObjectId(id)}, function(err, doc){
+        res.json(doc); // sending back to the controller the deleted object
+    })
+})
+
